d88a6b97bb3e52dbcffc5fef1bb9be1eb7cea70a
J. Daniel Ashton <jdashton@ashtonfam.org>
2019-12-12 12:59:25 -0500

    Empty framework ready for Day 1 Part 1

--- src/Advent01.php
+++ src/Advent01.php
@@ -5,15 +5,11 @@ final class Advent01
 {
     public static function fuelFor(int $mass): int
     {
-	    $calcAmt = intdiv($mass, 3) - 2;
-	    return $calcAmt > 0 ? $calcAmt + Advent01::fuelFor($calcAmt) : 0;
+	    # Implement this for part 1.
+	    # Enhance it for part 2.
     }
     public static function totalFuel(array $masses): int
     {
-	    $total_fuel = 0;
-	    foreach ($masses as $mass){
-		    $total_fuel = $total_fuel + self::fuelFor($mass);
-	    }
-	    return $total_fuel;
+	    # Implement this for part 2.
     }
 }
--- tests/Advent01Test.php
+++ tests/Advent01Test.php
@@ -5,36 +5,41 @@ use PHPUnit\Framework\TestCase;
 
 final class Advent01Test extends TestCase
 {
-    public function testFuelFor12Is2(): void
+    public function testFuelFor12(): void
     {
 	    $this->assertEquals(
 		    2,
 		    Advent01::fuelFor(12)
 	    );
     }
-    public function testFuelFor14Is2(): void
+    public function testFuelFor14(): void
     {
 	    $this->assertEquals(
 		    2,
 		    Advent01::fuelFor(14)
 	    );
     }
-    public function testFuelFor1969Is966(): void
+    public function testFuelFor1969(): void
     {
 	    $this->assertEquals(
-		    966,
+		    # 966, for part 2
+		    654,
 		    Advent01::fuelFor(1969)
 	    );
     }
-    public function testFuelFor100756Is50346(): void
+    public function testFuelFor100756(): void
     {
 	    $this->assertEquals(
-		    50346,
+		    # 50346, for part 2
+		    33583,
 		    Advent01::fuelFor(100756)
 	    );
     }
-    public function testTotalFuelFor12And14Is4(): void
+    public function testTotalFuelFor12And14(): void
     {
+	    $this->markTestSkipped(
+		    'Not applicable to part 1'
+	    );
 	    $this->assertEquals(
 		    4,
 		    Advent01::totalFuel([12, 14])
@@ -42,6 +47,9 @@ final class Advent01Test extends TestCase
     }
     public function testTotalFuelFor4Examples(): void
     {
+	    $this->markTestSkipped(
+		    'Not applicable to part 1'
+	    );
 	    $this->assertEquals(
 		    4 + 966 + 50346,
 		    Advent01::totalFuel([12, 14, 1969, 100756])
